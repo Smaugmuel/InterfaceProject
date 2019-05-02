626f319f75c9990e78bb19899741f6076902da6c
nistOne <mackan.holmberg@gmail.com>
2019-05-02 16:25:12 +0200

    Avoids multiple waypoints at the same position, due to overlapping planks.

--- Assets/WayPoint/spawner.cs
+++ Assets/WayPoint/spawner.cs
@@ -18,11 +18,17 @@ public class spawner : MonoBehaviour
             Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
 
             Ray ray = Camera.main.ScreenPointToRay(mousePos);
-            RaycastHit hit;
-            
-            if (Physics.Raycast(ray, out hit, 1000f, 9))
+            RaycastHit[] hits = Physics.RaycastAll(ray, 100f, 9);
+
+            hits = sortHitList(hits);
+
+            // If hit
+            if (hits.Length > 0)
             {
-                SpawnWaypoint(hit.point);
+                for (int i = 0; i < hits.Length; i++)
+                {
+                    SpawnWaypoint(hits[i].point);
+                }
             }
         }
     }
@@ -30,5 +36,55 @@ public class spawner : MonoBehaviour
     void SpawnWaypoint(Vector3 pos)
     {
         Instantiate(waypoint_model, pos + new Vector3(0f, 1f, 0f), Quaternion.identity);
+    }
+
+    RaycastHit[] sortHitList(RaycastHit[] hits)
+    {
+        const float minimumDistance = 1f;
+        int realHitCount = 0;
+        
+
+        // Sort list, Bubble sort <3
+        for (int i = 0; i < hits.Length; i++)
+        {
+            for (int j = i; j < hits.Length - 1; j++)
+            {
+                if (hits[j].point.y < hits[j + 1].point.y)
+                {
+                    RaycastHit temp = hits[j];
+                    hits[j] = hits[j + 1];
+                    hits[j + 1] = temp;
+                }
+            }
+        }
+
+        // Count real hit count
+        // Avoid hits that are to close to the one before it
+        // This avoids multiple waypoints at one "plank-intersection"
+        bool[] hitIsReal = new bool[hits.Length];
+        realHitCount++; // First hit always counts.
+        hitIsReal[0] = true;
+        for (int i = 1; i < hits.Length; i++)
+        {
+            hitIsReal[i] = Vector3.Distance(hits[i].point, hits[i - 1].point) >= minimumDistance;
+            if (hitIsReal[i])
+            {
+                realHitCount++;
+            }
+        }
+
+        RaycastHit[] hits_sorted = new RaycastHit[realHitCount];
+
+        // Copy list
+        int nextIndex = 0;
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hitIsReal[i])
+            {
+                hits_sorted[nextIndex++] = hits[i];
+            }
+        }
+
+        return hits_sorted;
     }
 }
