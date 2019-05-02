9de4bf4db826c707cf73a6b1ab02d162da0dfb98
nistOne <mackan.holmberg@gmail.com>
2019-05-02 15:44:48 +0200

    Added picking.

---
+++ Assets/WayPoint/spawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    public GameObject waypoint_model;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+
+            Ray ray = Camera.main.ScreenPointToRay(mousePos);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit, 1000f))
+            {
+                SpawnWaypoint(hit.point);
+            }
+        }
+    }
+
+    void SpawnWaypoint(Vector3 pos)
+    {
+        Instantiate(waypoint_model, pos + new Vector3(0f, 1f, 0f), Quaternion.identity);
+    }
+}
