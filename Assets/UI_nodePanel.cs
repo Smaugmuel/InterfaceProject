6ec6dc7925a8671182e2a6ee3e0fd0e5b6053969
tofb15 <fas_th@hotmail.com>
2019-05-02 16:54:18 +0200

    no message

--- Assets/UI_nodePanel.cs
+++ Assets/UI_nodePanel.cs
@@ -5,7 +5,24 @@ using UnityEngine;
 public class UI_nodePanel : MonoBehaviour
 {
     NodeSystem ns;
-    
+
+    [SerializeField]
+    GameObject nodeItem_Prefab;
+
+    public void UpdateUI()
+    {
+        GameObject[] items = GameObject.FindGameObjectsWithTag("ui_node_item");
+        for (int i = 0; i < items.Length; i++)
+        {
+            Destroy(items[i]);
+        }
+
+        for (int i = 0; i < ns.Nodes.Count; i++)
+        {
+            Instantiate(nodeItem_Prefab, transform);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
