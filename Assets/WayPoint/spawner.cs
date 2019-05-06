d527a27d64bb3d910387950c1a32e9f9760aac41
nistOne <mackan.holmberg@gmail.com>
2019-05-06 20:47:35 +0200

    Added side camera

--- Assets/WayPoint/spawner.cs
+++ Assets/WayPoint/spawner.cs
@@ -6,12 +6,16 @@ public class spawner : MonoBehaviour
 
     [SerializeField]
     public GameController gc;
+
+    public Camera sideCamera;
+
+    private Vector3 waypointPlaceOffset = new Vector3(0f, 1f, 0f);
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //
     }
 
     // Update is called once per frame
@@ -29,6 +33,12 @@ public class spawner : MonoBehaviour
             {
                 hits = sortHitList(hits);
 
+                // If multiple
+                if (hits.Length > 1)
+                {
+                    moveSideCamera(getAveragePos(hits));
+                }
+
                 for (int i = 0; i < hits.Length; i++)
                 {
                     SpawnWaypoint(hits[i].point);
@@ -39,7 +49,7 @@ public class spawner : MonoBehaviour
 
     void SpawnWaypoint(Vector3 pos)
     {
-        pos += new Vector3(0f, 1f, 0f);
+        pos += waypointPlaceOffset;
         Instantiate(waypoint_model, pos, Quaternion.identity);
         NodeSystem ns = gc.getNodeSystem();
         ns.AddNode(pos.x, pos.y, pos.z);
@@ -93,5 +103,27 @@ public class spawner : MonoBehaviour
         }
 
         return hits_sorted;
+    }
+
+    Vector3 getAveragePos(RaycastHit[] hits)
+    {
+        Vector3 average = hits[0].point;
+
+        for (int i = 1; i < hits.Length; i++)
+        {
+            average += hits[i].point;
+        }
+        average /= hits.Length;
+
+        return average;
+    }
+
+    void moveSideCamera(Vector3 lookAt)
+    {
+        lookAt += waypointPlaceOffset;
+        Vector3 dirFromOrigo = new Vector3(lookAt.x, 0f, lookAt.y).normalized * 6f;
+
+        sideCamera.transform.position = lookAt + dirFromOrigo + new Vector3(0, 3f, 0f);
+        sideCamera.transform.LookAt(lookAt);
     }
 }
