9bf39a1a050f9b87567cb0695f44d504d01b59b7
nistOne <mackan.holmberg@gmail.com>
2019-05-07 15:14:28 +0200

    Fixed side camera picking

--- Assets/Scripts/SideCameraPicker.cs
+++ Assets/Scripts/SideCameraPicker.cs
@@ -5,16 +5,22 @@ using UnityEngine.UI;
 public class SideCameraPicker : MonoBehaviour, IPointerClickHandler
 {
     public Camera camera;
+
+    public Canvas canvas;
     public RectTransform textureTransform;
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        Vector2 localClick = new Vector2(eventData.position.x - (textureTransform.position.x + textureTransform.rect.xMin), eventData.position.y - (textureTransform.position.y + textureTransform.rect.yMin));
-        //RectTransformUtility.ScreenPointToLocalPointInRectangle(textureTransform, eventData.position, Camera.main, out localClick);
-        //localClick *= new Vector2(1f, -1f);
+        float realWidth = textureTransform.rect.width * canvas.scaleFactor;
+        float realHegiht = textureTransform.rect.height * canvas.scaleFactor;
 
-        localClick /= new Vector2(textureTransform.rect.width, textureTransform.rect.height);
+        // Calculate local point of click
+        Vector2 localClick = new Vector2(eventData.position.x - (textureTransform.position.x - (realWidth/2f)), eventData.position.y - (textureTransform.position.y - (realHegiht/2f)));
 
+        // Convert x and y to [0f;1f]
+        localClick /= new Vector2(realWidth, realHegiht);
+
+        // Shoot ray from side camera
         Ray ray = camera.ViewportPointToRay(localClick);
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, 1000f))
