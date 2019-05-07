3adc0e7ed2e1b7396fd1c7eee20aa9922cdb9691
Lhure <maxjonilsson@gmail.com>
2019-05-07 14:26:25 +0200

    idk

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,11 +6,6 @@ public class GameController : MonoBehaviour
 {
     NodeSystem ns = new NodeSystem();
 
-    float nextUpdate = 1;
-
-    [SerializeField]
-    UI_nodePanel ui_nodePanel;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -28,13 +23,7 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //nextUpdate -= Time.deltaTime;
-        //if(nextUpdate < 0)
-        //{
-        //    nextUpdate = 1;
-        //    ns.AddNode(new NodeSystem.Node(Random.Range(-100,100), Random.Range(-100, 100), Random.Range(-100, 100)));
-        //    ui_nodePanel.UpdateUI();
-        //}
+        
     }
 
     public NodeSystem getNodeSystem()
