d7f468d8f7be5935d36dfe2751cf6994537e3c75
Lhure <maxjonilsson@gmail.com>
2019-05-09 14:54:21 +0200

    removed coments

--- Assets/Scripts/ChangeImagePosToMouse.cs
+++ Assets/Scripts/ChangeImagePosToMouse.cs
@@ -27,47 +27,16 @@ public class ChangeImagePosToMouse : MonoBehaviour
         //if (Input.GetMouseButtonDown(0))
         if (sideCamerPos != gameObjectB.transform.position)
         {
-            //transform.position = Input.mousePosition;
-
-            // cam pos = cam UI pos
-            // mouse pos
-
-            // 
-            //transform.position;
             Vector3 camPos = mainCamera.WorldToScreenPoint(gameObjectB.transform.position);
-
-
-            //Vector3 rotateDir = Input.mousePosition - transform.position;
+            
             Vector3 rotateDir = camPos - Input.mousePosition;
-            //Vector3 rotateDir = Input.mousePosition - camPos;
-            //Vector3 rotateDir = mainCamera.WorldToScreenPoint(Input.mousePosition) - camPos;
-            //Vector3 rotateDir = mainCamera.WorldToScreenPoint(Input.mousePosition) - transform.position;
-            //Vector3 rotateDir = (Input.mousePosition) - transform.position;
-            //rotateDir = rotateDir.normalized;
 
             float rotAngle = Mathf.Atan2(rotateDir.y, rotateDir.x) * Mathf.Rad2Deg;
             Quaternion newRot = Quaternion.AngleAxis(rotAngle + 90, Vector3.forward);
 
-            //float newRot = Quaternion.Angle(gameObjectB.transform.rotation, transform.r);
-
-            //transform.rotation = Quaternion.LookRotation(Input.mousePosition, Vector3.up);
-            //transform.rotation = Quaternion.LookRotation(rotateDir, Vector3.up);
-            //transform.Rotate(0, 90, 45);
-
-            //transform.position = camPos;
             transform.rotation = newRot;
             transform.position = Input.mousePosition;
-
-
-
-
-
-            //Quaternion temp = new Vector3(0, 0, gameObjectB.transform.rotation.eulerAngles.y);
-            //transform.rotation.eulerAngles = temp;
-            //pickingHandler.cameraLookAt;
-            //transform.LookAt(pickingHandler.cameraLookAt);
-            //transform.LookAt(centerPos);
-            //transform.Rotate(0, 90,  90 + Mathf.Asin(rotateDir.x));
+            
             sideCamerPos = gameObjectB.transform.position;
         }
 
