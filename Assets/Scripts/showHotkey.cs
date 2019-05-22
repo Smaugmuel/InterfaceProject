bc5cda9723ce018a580819c58147fb20f483f74b
Lhure <maxjonilsson@gmail.com>
2019-05-22 11:59:20 +0200

    implemented

---
+++ Assets/Scripts/showHotkey.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class showHotkey : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position = new Vector2(-200, -200);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.mousePosition.x < 100 && Input.mousePosition.y > Screen.height - 100)
+        {
+            gameObject.transform.position = new Vector2(120, Screen.height - 100);
+        }
+        else
+        {
+            gameObject.transform.position = new Vector2(-200, -200);
+        }
+    }
+}
